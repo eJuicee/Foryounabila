4d2767841c604f59eac8799b6dd325f99818d2f7
eJuicee <andre.ansyah3016@gmail.com>
2025-04-03 23:29:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,42 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+
+    texts: [
+        "🥰",     
+        "🫰",  
+        "🩷",
+        "💕",
+        "💖",
+        "🫶",
+        "💕",
+        "💖",
+        "🫶",
+       
+       
+    ],
+    imgs: {
+        "🥰": "./imgs/bila1.jpg",
+        "🫰": "./imgs/bila2.jpg",
+        "🩷": "./imgs/bila3.jpg",
+        "💕": "./imgs/bila4.jpg",
+        "💖": "./imgs/bila5.jpg",
+        "🫶": "./imgs/bila6.jpg",
+        "💕": "./imgs/bila7.jpg",
+        "💖": "./imgs/bila8.jpg",
+        "🫶": "./imgs/bila9.jpg",
+        
+    },
+    
+    desc: {
+        turn_on: "NYALAIN LAMPUNYAAA SAYANGGG",
+        play: "NYALAIN MUSIKNYA YAAA",
+        bannar_coming: "AYO KAMU DEKOR SAYANG",
+        balloons_flying: "TERBANGIN BALONNYA SAYANG",
+        cake_fadein: "AMBIL KUENYA SAYANG",
+        light_candle: "NYALAIN LILINNYAA SAYANG",
+        story: "Semoga Kamu Suka Ya Sayang💖"
+    },
+
+    // 结束语
+    loveText: '"Selamat ulang tahun, sayang! Hari ini adalah hari spesial kamu, Semoga tahun ini membawa kebahagiaan yang berlimpah, kesuksesan, dan kesehatan yang selalu menemanimu. Makasih ya udah mau nemenin aku. Aku akan selalu ada di samping kamu, dan mencintaimu lebih dari apa pun. Manja kamu jangan ilang ya sayang hehe. Love you always!" 💖'
+};
